3e340b61f50e3d4aef80d8e83fd8f7b9d2e37780
本川　晴彦 <harx6009@icloud.com>
2025-06-07 14:52:02 +0900

    SplitWatchScreenウィジェットを追加し、色に基づくデータを表示するためのConsumerを実装

---
+++ lib/presentation/screens/split_watch/screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_watch_many_sample/presentation/shared/color_list_tile.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/blue_5_seconds.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/red_1_seconds.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/yellow_10_seconds.dart';
+
+class SplitWatchScreen extends StatelessWidget {
+  const SplitWatchScreen({super.key});
+
+  static const String path = '/split_watch';
+  static const String name = 'SplitWatchScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(name),
+      ),
+      body: Center(
+          child: Column(
+        spacing: 20,
+        children: [
+          Consumer(builder: (context, ref, child) {
+            final asyncRed = ref.watch(red1SecondsProvider);
+            return switch (asyncRed) {
+              AsyncData(value: final red) => ColorListTile(color: red),
+              AsyncError() => const Text('Error occurred while fetching color'),
+              _ => const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  child: CircularProgressIndicator(),
+                ),
+            };
+          }),
+          Consumer(builder: (context, ref, child) {
+            final asyncBlue = ref.watch(blue5SecondsProvider);
+            return switch (asyncBlue) {
+              AsyncData(value: final blue) => ColorListTile(color: blue),
+              AsyncError() => const Text('Error occurred while fetching color'),
+              _ => const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  child: CircularProgressIndicator(),
+                ),
+            };
+          }),
+          Consumer(builder: (context, ref, child) {
+            final asyncYellow = ref.watch(yellow10SecondsProvider);
+            return switch (asyncYellow) {
+              AsyncData(value: final yellow) => ColorListTile(color: yellow),
+              AsyncError() => const Text('Error occurred while fetching color'),
+              _ => const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  child: CircularProgressIndicator(),
+                ),
+            };
+          }),
+        ],
+      )),
+    );
+  }
+}
