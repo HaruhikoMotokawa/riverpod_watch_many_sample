d27a0b727eb1f7541d2edbcef1c4dd197bd4bb1b
本川　晴彦 <harx6009@icloud.com>
2025-06-07 14:52:42 +0900

    CombinedProviderScreenウィジェットを追加し、色に基づくデータを表示するためのConsumerを実装

---
+++ lib/presentation/screens/combined_provider/provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/blue_5_seconds.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/red_1_seconds.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/yellow_10_seconds.dart';
+
+part 'provider.g.dart';
+
+typedef CombinedProviderScreenState = ({
+  MaterialColor red,
+  MaterialColor blue,
+  MaterialColor yellow,
+});
+
+@riverpod
+Future<CombinedProviderScreenState> combinedProviderScreenState(Ref ref) async {
+  final red = await ref.watch(red1SecondsProvider.future);
+  final blue = await ref.watch(blue5SecondsProvider.future);
+  final yellow = await ref.watch(yellow10SecondsProvider.future);
+
+  return (
+    red: red,
+    blue: blue,
+    yellow: yellow,
+  );
+}
---
+++ lib/presentation/screens/combined_provider/screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/combined_provider/provider.dart';
+import 'package:riverpod_watch_many_sample/presentation/shared/color_list_tile.dart';
+
+class CombinedProviderScreen extends ConsumerWidget {
+  const CombinedProviderScreen({super.key});
+
+  static const String path = '/combined_provider';
+  static const String name = 'CombinedProviderScreen';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncState = ref.watch(combinedProviderScreenStateProvider);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(name),
+        ),
+        body: Center(
+          child: switch (asyncState) {
+            AsyncData(value: final state) => Column(
+                spacing: 20,
+                children: [
+                  ColorListTile(color: state.red),
+                  ColorListTile(color: state.blue),
+                  ColorListTile(color: state.yellow),
+                ],
+              ),
+            AsyncError() => const Text('Error occurred while fetching data'),
+            _ => const CircularProgressIndicator(),
+          },
+        ));
+  }
+}
