a9c0fe9ad035a2b8af57b752a18b3ae9e71c12d5
本川　晴彦 <harx6009@icloud.com>
2025-06-07 14:51:27 +0900

    ColorListTileウィジェットを追加し、色に基づくタイトルとサブタイトルを表示

---
+++ lib/presentation/shared/color_list_tile.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ColorListTile extends StatelessWidget {
+  const ColorListTile({
+    required this.color,
+    super.key,
+  });
+
+  final MaterialColor color;
+
+  String get _colorName => switch (color) {
+        Colors.red => 'Red',
+        Colors.blue => 'Blue',
+        Colors.yellow => 'Yellow',
+        _ => 'Unknown Color',
+      };
+
+  String get _subtitle => switch (color) {
+        Colors.red => '取得までに1秒かかります',
+        Colors.blue => '取得までに5秒かかります',
+        Colors.yellow => '取得までに10秒かかります',
+        _ => 'unSupported Color',
+      };
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.color_lens),
+      title: Text('Color: $_colorName'),
+      subtitle: Text(_subtitle),
+      onTap: () {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Color: $_colorName'),
+            duration: const Duration(milliseconds: 500),
+          ),
+        );
+      },
+      tileColor: color,
+    );
+  }
+}
