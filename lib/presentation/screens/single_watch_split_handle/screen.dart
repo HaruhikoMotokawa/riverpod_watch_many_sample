2b6bbc7f120d034921290395ebf4eee3fb3adb31
本川　晴彦 <harx6009@icloud.com>
2025-06-07 14:52:16 +0900

    SingleWatchSplitHandleScreenウィジェットを追加し、色に基づくデータを表示するためのConsumerを実装

---
+++ lib/presentation/screens/single_watch_split_handle/screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_watch_many_sample/presentation/shared/color_list_tile.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/blue_5_seconds.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/red_1_seconds.dart';
+import 'package:riverpod_watch_many_sample/presentation/state/yellow_10_seconds.dart';
+
+class SingleWatchSplitHandleScreen extends ConsumerWidget {
+  const SingleWatchSplitHandleScreen({super.key});
+
+  static const String path = '/single_watch_split_handle';
+  static const String name = 'SingleWatchSplitHandleScreen';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncRed = ref.watch(red1SecondsProvider);
+    final asyncBlue = ref.watch(blue5SecondsProvider);
+    final asyncYellow = ref.watch(yellow10SecondsProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(name),
+      ),
+      body: Center(
+        child: Column(
+          spacing: 20,
+          children: [
+            switch (asyncRed) {
+              AsyncData(value: final red) => ColorListTile(color: red),
+              AsyncError() =>
+                const Text('Error occurred while fetching red color'),
+              _ => const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  child: CircularProgressIndicator(),
+                ),
+            },
+            switch (asyncBlue) {
+              AsyncData(value: final blue) => ColorListTile(color: blue),
+              AsyncError() =>
+                const Text('Error occurred while fetching blue color'),
+              _ => const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  child: CircularProgressIndicator(),
+                ),
+            },
+            switch (asyncYellow) {
+              AsyncData(value: final yellow) => ColorListTile(color: yellow),
+              AsyncError() =>
+                const Text('Error occurred while fetching yellow color'),
+              _ => const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 2),
+                  child: CircularProgressIndicator(),
+                ),
+            },
+          ],
+        ),
+      ),
+    );
+  }
+}
