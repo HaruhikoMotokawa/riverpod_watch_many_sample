fead91db998ca82f113a8d1b1e0845f929af9baa
本川　晴彦 <harx6009@icloud.com>
2025-06-07 14:53:00 +0900

    MainAppをConsumerWidgetに変更し、ルーター設定を追加。HomeScreenにアプリバーとナビゲーションリストを実装。

--- lib/presentation/screens/home/screen.dart
+++ lib/presentation/screens/home/screen.dart
@@ -1,16 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/combined_provider/screen.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/single_watch_split_handle/screen.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/split_watch/screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
     super.key,
   });
 
+  static const String path = '/home';
+  static const String name = 'home';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Column(
+          spacing: 6,
+          children: [
+            _ListTile(screenType: _ScreenType.splitWatch),
+            Divider(),
+            _ListTile(screenType: _ScreenType.singleWatchSplitHandle),
+            Divider(),
+            _ListTile(screenType: _ScreenType.combinedProvider),
+            Divider(),
+          ],
+        ),
       ),
     );
   }
+}
+
+class _ListTile extends StatelessWidget {
+  const _ListTile({required this.screenType});
+
+  final _ScreenType screenType;
+
+  String get _screenName => switch (screenType) {
+        _ScreenType.splitWatch => SplitWatchScreen.name,
+        _ScreenType.singleWatchSplitHandle => SingleWatchSplitHandleScreen.name,
+        _ScreenType.combinedProvider => CombinedProviderScreen.name,
+      };
+
+  String get _screenPath => switch (screenType) {
+        _ScreenType.splitWatch => SplitWatchScreen.path,
+        _ScreenType.singleWatchSplitHandle => SingleWatchSplitHandleScreen.path,
+        _ScreenType.combinedProvider => CombinedProviderScreen.path,
+      };
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('Go to $_screenName'),
+      trailing: const Icon(Icons.arrow_forward),
+      onTap: () {
+        context.push(_screenPath);
+      },
+    );
+  }
+}
+
+enum _ScreenType {
+  splitWatch,
+  singleWatchSplitHandle,
+  combinedProvider,
 }
---
+++ lib/router/router.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/combined_provider/screen.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/home/screen.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/single_watch_split_handle/screen.dart';
+import 'package:riverpod_watch_many_sample/presentation/screens/split_watch/screen.dart';
+
+part 'router.g.dart';
+
+@Riverpod(keepAlive: true)
+GoRouter router(Ref ref) => _goRouter;
+
+final _goRouter = GoRouter(
+  // アプリが起動した時
+  initialLocation: HomeScreen.path,
+  // パスと画面の組み合わせ
+  routes: [
+    // ---------------- //
+    // HomeScreen
+    // ---------------- //
+    GoRoute(
+      path: HomeScreen.path,
+      name: HomeScreen.name,
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: const HomeScreen(),
+        );
+      },
+    ),
+    // ---------------- //
+    // SplitWatchScreen
+    // ---------------- //
+    GoRoute(
+      path: SplitWatchScreen.path,
+      name: SplitWatchScreen.name,
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: const SplitWatchScreen(),
+        );
+      },
+    ),
+    // ---------------- //
+    // SingleWatchSplitHandleScreen
+    // ---------------- //
+    GoRoute(
+      path: SingleWatchSplitHandleScreen.path,
+      name: SingleWatchSplitHandleScreen.name,
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: const SingleWatchSplitHandleScreen(),
+        );
+      },
+    ),
+    // ---------------- //
+    // CombinedProviderScreen
+    // ---------------- //
+    GoRoute(
+      path: CombinedProviderScreen.path,
+      name: CombinedProviderScreen.name,
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: const CombinedProviderScreen(),
+        );
+      },
+    ),
+  ],
+  // 遷移ページがないなどのエラーが発生した時に、このページに行く
+  errorPageBuilder: (context, state) => MaterialPage(
+    key: state.pageKey,
+    child: Scaffold(
+      body: Center(
+        child: Text(state.error.toString()),
+      ),
+    ),
+  ),
+);
